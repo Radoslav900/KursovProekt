e7e3debfc229119bc62964a942ccdef1e00f7c7f
DESKTOP-I65T3K3\lenovo <realsteelatomst@gmail.com>
2024-02-04 19:02:50 +0200

    Tova sum napravil

---
+++ src/test/java/base/TestUtil.java
@@ -0,0 +1,78 @@
+package base;
+
+import com.google.j2objc.annotations.Property;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.bouncycastle.util.Properties;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class TestUtil extends DataProviders{
+    public WebDriver driver;
+    private String browser, targetURL;
+    private int implicitWait;
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+    @BeforeMethod
+    public void setupDriverAndOpenTargetURL() {
+        readConfig("src/test/resources/config.properties");
+        setupDriver();
+        //Implicit wait:
+        driver.manage().timeouts().implicitlyWait(Duration.from(Duration.ofSeconds(implicitWait)));
+        driver.get(targetURL);
+    }
+    @Parameters
+    private void readConfig(String pathToFile){
+        try{
+            FileInputStream fileInputStream = new FileInputStream(pathToFile);
+            Properties properties = new Properties();
+            properties.load(fileInputStream);
+            targetURL = properties.getProperty("url");
+            browser = properties.getProperty("browser");
+            implicitWait = Integer.parseInt(properties.getProperty("wait"));
+        }catch (IOException e){
+            System.out.println(e);
+        }
+    }
+
+    private void setupDriver(){
+        switch (browser){
+            case "safari":
+                driver = setupSafariDriver();
+                break;
+            case "firefox":
+                driver = setupFireFoxDriver();
+                break;
+            default:
+                driver = setupChromeDriver();
+        }
+    }
+
+    private WebDriver setupChromeDriver(){
+        WebDriverManager.chromedriver().setup();
+        return driver = new ChromeDriver();
+    }
+
+    private WebDriver setupSafariDriver(){
+        WebDriverManager.safaridriver().setup();
+        return new SafariDriver();
+    }
+
+    private WebDriver setupFireFoxDriver(){
+        WebDriverManager.firefoxdriver().setup();
+        return new FirefoxDriver();
+    }
+}
